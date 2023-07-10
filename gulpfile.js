43d3afc4e49995b0f367d6c4d4610c88a0916995
tmdwnsyang <95985696+tmdwnsyang@users.noreply.github.com>
2023-07-10 10:14:59 -0700

    minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-// function defaultTask(cb) {
-//   // place code for your default task here
-//   cb();
-// }
 
-// exports.default = defaultTask
 const gulp = require('gulp')
 const minify = require('gulp-minify');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+  return gulp.src('docs/css/styles.css')
+  .pipe(cleanCSS({compatibility:'ie8'}))
+  .pipe(gulp.dest('docs/css'));
+})
+
  
 gulp.task('compress', function(done) {
   gulp.src(['docs/js/*.js', 'docs/js/*.mjs'])
